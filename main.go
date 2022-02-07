1fa6f55fa6568d3c0a399b169b7d6554eee096e0
Daniel J Blueman <daniel@quora.org>
2022-02-07 13:36:56 +0800

    Make output more informitive

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func dump(out traceroute.TracerouteResult) {
    t := time.Now().Format(time.RFC1123Z)
-   fmt.Printf("change seen at %s\n", t)
+   fmt.Printf(" at %s\n", t)
 
    for _, hop := range out.Hops {
       fmt.Printf("%-3d %v (%v)  %v\n", hop.TTL, hop.HostOrAddressString(), hop.AddressString(), hop.ElapsedTime)
@@ -21,11 +21,13 @@ func dump(out traceroute.TracerouteResult) {
 
 func diff(current, last traceroute.TracerouteResult) bool {
    if len(current.Hops) != len(last.Hops) {
+      fmt.Printf("number of hops went from %d to %d", len(last.Hops), len(current.Hops))
       return true
    }
 
    for i := range current.Hops {
-      if current.Hops[i].Address != last.Hops[i].Address {
+      if current.Hops[i].AddressString() != last.Hops[i].AddressString() {
+         fmt.Printf("hop %d changed from %s to %s", i, last.Hops[i].AddressString(), current.Hops[i].AddressString())
          return true
       }
    }
